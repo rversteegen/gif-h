a72561b2439bf845b7f49f9ee3b374efc9ade916
Ralph Versteegen <teeemcee@gmail.com>
2017-12-31 00:35:36 +1300

    Fix GifRGBA.comps() not allowing reordering of RGBA, and was unnecessarily slow

--- gif.h
+++ gif.h
@@ -76,16 +76,9 @@ const int kGifTransIndex = 0;
 struct GifRGBA
 {
     uint8_t r, g, b, a;
-    uint8_t comps(int i)
+    uint8_t& comps(int comp)
     {
-        switch(i)
-        {
-        case 0: return r;
-        case 1: return g;
-        case 2: return b;
-        case 3: return a;
-        }
-        return 0;
+        return ((uint8_t*)this)[comp];
     }
 };
 
